0951ba6feee4b1d5566d18569b720c4ab2f65b12
sekiguchi <sekiguchi@framelunch.jp>
2023-01-16 19:48:34 +0900

    first commit

---
+++ pages/tag/[id].js
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { client } from "../../libs/client";
+
+export default function CategoryId({ blog }) {
+  // タグに紐付いたコンテンツがない場合に表示
+  if (blog.length === 0) {
+    return <div>ブログコンテンツがありません</div>;
+  }
+  return (
+    <div>
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "tags" });
+
+  const paths = data.contents.map((content) => `/tag/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "blog", queries: { filters: `tags[contains]${id}` } });
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
